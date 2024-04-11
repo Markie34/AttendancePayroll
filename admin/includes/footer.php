c0d614a6a7cfe576e8ae8d5b3c4f91c3135ddee1
Ranvoxrat <markfeliciano233@gmail.com>
2024-04-11 15:38:13 +0800

    Latest Project

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; v0.2 2024 by Mark Feliciano</strong>
+</footer>
